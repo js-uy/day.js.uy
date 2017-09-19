03e363f50b4a4f379c5cac19885b13e3c620434f
Ignacio Álvarez <ignacioalvarez92@gmail.com>
2017-09-18 21:57:59 -0300

    New build

--- docs/_next/0929f7bd-1f7b-45de-9e5d-f789350a1b4f/page/_error/index.js
+++ docs/_next/0929f7bd-1f7b-45de-9e5d-f789350a1b4f/page/_error/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/_error', function() {
-            var comp = module.exports=webpackJsonp([4],[],[400]);
+            var comp = module.exports=webpackJsonp([4],[],[409]);
             return { page: comp.default }
           })
         
