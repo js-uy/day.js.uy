b215cc7e7f2f4d67bd3467485309baf156c35e02
Rodrigo Espinosa Curbelo <espinosacurbelo@gmail.com>
2017-10-05 15:35:43 -0300

    Updates

--- _next/2e41e7c5-f8eb-40b9-a758-2659e7e3426a/page/index.js
+++ _next/2e41e7c5-f8eb-40b9-a758-2659e7e3426a/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([3],[],[448]);
+            var comp = module.exports=webpackJsonp([3],[],[449]);
             return { page: comp.default }
           })
         
