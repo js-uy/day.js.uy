e69c809bf65ceeceeaabe88acc5463b3fc38fa06
Rodrigo Espinosa Curbelo <espinosacurbelo@gmail.com>
2017-10-04 22:16:25 -0300

    Fix styles in Venue

--- components/VenueHero.js
+++ components/VenueHero.js
@@ -20,12 +20,12 @@ const VenuePics = styled.figure`
 `;
 
 const VenueInfo = styled.div`
+  align-items: flex-start;
   color: ${theme.white};
   display: flex;
   flex-direction: column;
   flex: 233;
-  justify-content: center;
-  align-items: flex-start;
+  justify-content: flex-start;
   padding: ${theme.gridSpacing * 2}px;
 
   @media (max-width: ${theme.mobileThreshold}px) {
@@ -37,14 +37,32 @@ const Link = styled.a`color: ${theme.yellow};`;
 
 const H2 = styled.h2`
   color: ${theme.lightblue};
+  font-weight: normal;
   text-transform: uppercase;
 `;
 
 const VenueGlow = styled(Glow)`
+  margin-top: 88px;
+  margin-bottom: 80px;
   transform: translateX(-15px);
+
   @media (max-width: ${theme.mobileThreshold}px) {
     transform: translateX(-10px);
   }
+`;
+
+const Text = styled.p`font-size: 1rem;`;
+
+const SeparationLine = styled.div`
+  height: 1px;
+  background: #fff;
+  margin: 70px 0 40px 0;
+  width: 40px;
+`;
+
+const ShowMap = styled(Link)`
+  font-weight: normal;
+  text-decoration: none;
 `;
 
 export default () => (
@@ -55,20 +73,30 @@ export default () => (
       </Desktop>
       <VenueInfo>
         <VenueGlow src="/static/venue.svg" color={theme.lightblue} />
+
         <div>
           <H2>WHERE</H2>
-          <p>
+          <Text>
             <strong>Esplendor Hotel</strong>
-          </p>
-          <p>Manuel Errazquin 2370</p>
-          <p>11300 - Montevideo, Uruguay</p>
+            <br />
+            Manuel Errazquin 2370<br />
+            11300 - Montevideo, Uruguay
+          </Text>
         </div>
-        <Link
+
+        <div>
+          <H2>WHEN</H2>
+          <Text>November 24 & 25, 2017</Text>
+        </div>
+
+        <SeparationLine />
+
+        <ShowMap
           href="https://www.google.com.uy/maps/place/Esplendor+Montevideo+A+Wyndham+Grand+Hotel/@-34.9113744,-56.1415407,14z/data=!4m8!1m2!2m1!1sesplendor+hotel!3m4!1s0x0:0x40a5c2bc61e23a5d!8m2!3d-34.9283442!4d-56.1601996"
           target="_blank"
         >
-          Show Map
-        </Link>
+          Show Map →
+        </ShowMap>
       </VenueInfo>
     </Container>
   </Hero>
