b7ebb1bacbddaa70a047ff86398be32bbaa31dd1
Gabriel Chertok <gchertok@gmail.com>
2017-10-16 19:12:48 -0300

    Updates

--- _next/d7a96b23-c716-47d1-a3d3-ab309a0646a7/page/_error/index.js
+++ _next/d7a96b23-c716-47d1-a3d3-ab309a0646a7/page/_error/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/_error', function() {
-            var comp = module.exports=webpackJsonp([4],[],[450]);
+            var comp = module.exports=webpackJsonp([4],[],[600]);
             return { page: comp.default }
           })
         
