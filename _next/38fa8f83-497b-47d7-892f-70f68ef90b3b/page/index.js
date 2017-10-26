b3c2a7dea28224fb8ec7ab570f663a60a223f3ac
Ignacio Álvarez <ignacioalvarez92@gmail.com>
2017-10-25 21:07:57 -0300

    Updates

--- _next/38fa8f83-497b-47d7-892f-70f68ef90b3b/page/index.js
+++ _next/38fa8f83-497b-47d7-892f-70f68ef90b3b/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([3],[],[600]);
+            var comp = module.exports=webpackJsonp([3],[],[614]);
             return { page: comp.default }
           })
         
