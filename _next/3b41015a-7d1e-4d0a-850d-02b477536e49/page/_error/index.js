619a8850f384196a038f3ad545d63108e98c5648
Ignacio Álvarez <ignacioalvarez92@gmail.com>
2017-11-03 00:29:09 -0300

    Updates

--- _next/3b41015a-7d1e-4d0a-850d-02b477536e49/page/_error/index.js
+++ _next/3b41015a-7d1e-4d0a-850d-02b477536e49/page/_error/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/_error', function() {
-            var comp = module.exports=webpackJsonp([4],[],[615]);
+            var comp = module.exports=webpackJsonp([4],[],[617]);
             return { page: comp.default }
           })
         
--- _next/3b41015a-7d1e-4d0a-850d-02b477536e49/page/index.js
+++ _next/3b41015a-7d1e-4d0a-850d-02b477536e49/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([3],[],[614]);
+            var comp = module.exports=webpackJsonp([3],[],[616]);
             return { page: comp.default }
           })
         
