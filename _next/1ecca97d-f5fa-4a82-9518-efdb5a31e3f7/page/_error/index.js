b5ba9da29d9f9c69180e17d040458a909bda1b15
Nacho Álvarez <ignacioalvarez92@gmail.com>
2017-11-08 12:53:04 -0300

    Updates

--- _next/1ecca97d-f5fa-4a82-9518-efdb5a31e3f7/page/_error/index.js
+++ _next/1ecca97d-f5fa-4a82-9518-efdb5a31e3f7/page/_error/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/_error', function() {
-            var comp = module.exports=webpackJsonp([4],[],[617]);
+            var comp = module.exports=webpackJsonp([4],[],[620]);
             return { page: comp.default }
           })
         
--- _next/1ecca97d-f5fa-4a82-9518-efdb5a31e3f7/page/index.js
+++ _next/1ecca97d-f5fa-4a82-9518-efdb5a31e3f7/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([3],[],[616]);
+            var comp = module.exports=webpackJsonp([3],[],[619]);
             return { page: comp.default }
           })
         
