914f7055596a6e3b8bc727ceb03e4e401fee80f6
Gabriel Chertok <gchertok@gmail.com>
2017-11-07 21:53:58 -0300

    Updates

--- _next/3a13abf5-2ede-4370-bc5e-bc286dd03517/page/index.js
+++ _next/3a13abf5-2ede-4370-bc5e-bc286dd03517/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([3],[],[616]);
+            var comp = module.exports=webpackJsonp([3],[],[602]);
             return { page: comp.default }
           })
         
